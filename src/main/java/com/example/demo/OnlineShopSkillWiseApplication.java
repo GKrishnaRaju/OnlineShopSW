fe22a73ae8a7208ccfe43633823a1c30e0261366
krishna <krishnashu@yahoo.com>
2019-05-05 11:31:18 +0530

    first

---
+++ src/main/java/com/example/demo/OnlineShopSkillWiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopSkillWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopSkillWiseApplication.class, args);
+	}
+
+}
